2d67f771d2c7ad2cb79b72096a7c4c285fc0f5b4
Henry Wiseman <166332161+hwise1@users.noreply.github.com>
2024-05-28 22:53:17 -0700

    Update summary.R

---
+++ summary.R
@@ -0,0 +1,28 @@
+library(tidyr)
+library(dplyr)
+
+url4 <- "https://github.com/melaniewalsh/Neat-Datasets/blob/main/us-prison-pop.csv?raw=true"
+prison_county_state <- read.csv(url4)
+
+sum_black_pop <- sum(prison_county_state$black_prison_pop, na.rm = TRUE)
+sum_white_pop <- sum(prison_county_state$white_prison_pop, na.rm = TRUE)
+sum_total_pop <- sum(prison_county_state$total_prison_pop, na.rm = TRUE)
+sum_latinx_pop <- sum(prison_county_state$latinx_prison_pop, na.rm = TRUE)
+latinx_area_pop <- sum(prison_county_state$latinx_pop_15to64, na.rm = TRUE)
+black_area_pop <- sum(prison_county_state$black_pop_15to64, na.rm = TRUE)
+white_area_pop <- sum(prison_county_state$white_pop_15to64, na.rm = TRUE)
+
+
+
+latinx_in_prison_percent <- sum_latinx_pop / latinx_area_pop
+
+prison_black_percentage <- sum_black_pop / sum_total_pop
+  
+prison_latinx_percentage <- sum_latinx_pop / sum_total_pop
+
+black_in_prison_percent <- sum_black_pop / black_area_pop
+
+prison_white_percentage <- sum_white_pop / sum_total_pop
+
+white_in_prison_percent <- sum_white_pop / white_area_pop
+
