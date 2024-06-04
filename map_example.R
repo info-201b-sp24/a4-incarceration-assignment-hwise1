40e22b76e633965bbb427c890dbbab6c7f9224d2
Henry Wiseman <166332161+hwise1@users.noreply.github.com>
2024-06-03 23:37:14 -0700

    Update map_example.R

--- map_example.R
+++ map_example.R
@@ -26,16 +26,17 @@ data_2016_by_state$state <- tolower(state.name[match(data_2016_by_state$state, s
 
 states_map <- map_data("state")
 
-data_2016_by_state <- counties_map %>%
+data_2016_by_state <- states_map %>%
   left_join(data_2016_by_state, by = c("region" = "state"))
 
 library(ggplot2)
 ggplot(data_2016_by_state, aes(map_id = region, fill = ratio)) +
-  geom_map(map = US_map, aes(x = long, y = lat, map_id = region)) +
-  expand_limits(x = counties_map$long, y = counties_map$lat) +
+  geom_map(map = states_map, aes(x = long, y = lat, map_id = region)) +
+  expand_limits(x = states_map$long, y = states_map$lat) +
   scale_fill_gradient(low = "lightblue", high = "darkblue", na.value = "grey50", name = "Percentage") +
   labs(title = "Percentage of Population in Prison by State",
        x = "",
        y = "") +
   theme_void() +
   theme(legend.position = "right")
+
